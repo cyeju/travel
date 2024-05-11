c3160838b61e30340db11588b5f51f0b4c0c5bac
cyeju <yeju1019@gmail.com>
2024-05-12 01:05:43 +0900

    chore: page 정리 및 css 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://cyeju.github.io/travel/"
+      : "",
 };
 
 export default nextConfig;
